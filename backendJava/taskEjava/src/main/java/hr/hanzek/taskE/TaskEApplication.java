688ec674670a00d53c89b2ebe0d815ff35d73af5
JurajHanzek <hanzekj@hotmail.com>
2022-10-27 23:29:03 +0200

    setup java

---
+++ backendJava/taskEjava/src/main/java/hr/hanzek/taskE/TaskEApplication.java
@@ -0,0 +1,13 @@
+package hr.hanzek.taskE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskEApplication.class, args);
+	}
+
+}
---
+++ backendJava/taskEjava/src/test/java/hr/hanzek/taskE/TaskEApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.hanzek.taskE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
